7dbed7abd8a3393312e08899478adce400e32b79
Julien Adam <julien.adam@arcodia.fr>
2022-05-06 09:56:35 +0200

    Adding unit tests to angular app

--- SampleJwtFront/src/app/_services/token-storage.service.spec.ts
+++ SampleJwtFront/src/app/_services/token-storage.service.spec.ts
@@ -13,4 +13,15 @@ describe('TokenStorageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return token after storing it', () => {
+    service.saveToken("foo");
+    expect(service.getToken()).toBe("foo");
+  });
+
+  it('should return user data after storing it', () => {
+    var u = { name: "blah", roles: ["Admin", "User"] };
+    service.saveUser(u);
+    expect(service.getUser()).toEqual(u);
+  });
 });
--- SampleJwtFront/src/app/app.component.spec.ts
+++ SampleJwtFront/src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SampleJwtFront'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('SampleJwtFront');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SampleJwtFront app is running!');
-  });
 });
